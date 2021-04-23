5fee9882ccebedd3704ffe564823545e5788cd43
j-miko <83086111+j-miko@users.noreply.github.com>
2021-04-23 22:36:21 +0200

    First benchmark submission with new model

---
+++ submission/model.py
@@ -0,0 +1,43 @@
+from sklearn import tree
+import numpy as np
+import pickle
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+class ModelRNN:
+    def __init__(self, model_file_path):
+        self.model_file_path = model_file_path
+        model_data = pickle.load(open(self.model_file_path, "rb"))
+        model = keras.Sequential.from_config(model_data['config'])
+        model.set_weights(model_data['weights'])
+        
+    def predict(self, input_df):
+        return model.predict(input_df)
+
+class BaselineModel:
+    def __init__(self, model_file_path):
+        self.model_file_path = model_file_path
+
+    def vectorize_sequences(self, sequence_array):
+        vectorize_on_length = np.vectorize(len)
+        return np.reshape(vectorize_on_length(sequence_array), (-1, 1))
+
+    def train(self, df_train):
+        X = self.vectorize_sequences(df_train['sequence'].to_numpy())
+        y = df_train['mean_growth_PH'].to_numpy()
+
+        model = tree.DecisionTreeRegressor()
+        model.fit(X, y)
+
+        with open(self.model_file_path, 'wb') as model_file:
+            pickle.dump(model, model_file)
+
+    def predict(self, df_test):
+        with open(self.model_file_path, 'rb') as model_file:
+            model: tree.DecisionTreeRegressor = pickle.load(model_file)
+
+        X = df_test['sequence'].to_numpy()
+        X_vectorized = self.vectorize_sequences(X)
+        return model.predict(X_vectorized)
---
+++ submission/predict.py
@@ -0,0 +1,24 @@
+import argparse
+import pandas as pd
+from model import BaselineModel
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_csv', default='input.csv')
+args = parser.parse_args()
+
+# Config
+output_file_path = 'predictions.csv'
+
+# Load input.csv
+with open(args.input_csv) as input_csv:
+    df = pd.read_csv(input_csv)
+
+# Run predictions
+# y_predictions = BaselineModel(model_file_path='src/model.pickle').predict(df)
+y_predictions = ModelRNN(model_file_path='src/model.pkl').predict(df)
+
+# Save predictions to file
+df_predictions = pd.DataFrame({'prediction': y_predictions})
+df_predictions.to_csv(output_file_path, index=False)
+
+print(f'{len(y_predictions)} predictions saved to a csv file')
