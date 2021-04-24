4d7ce44d84e11819c994379c376a3c6084adb459
mateuszw314 <53479545+mateuszw314@users.noreply.github.com>
2021-04-25 00:55:40 +0200

    Model 4 src

--- src/predict.py
+++ src/predict.py
@@ -1,7 +1,7 @@
 import argparse
 import pandas as pd
 import numpy as np
-from model import ModelADA
+from model import ModelRNN
 from tensorflow.keras.preprocessing import sequence
 
 
@@ -29,8 +29,7 @@ df = pd.DataFrame(sequence_encoded)
 
 # Run predictions
 # y_predictions = BaselineModel(model_file_path='src/model.pickle').predict(df)
-# y_predictions = ModelRNN(model_file_path='src/model_3.pkl').predict(df)
-y_predictions = ModelADA(model_file_path='src/model_ada.pkl').predict(df)
+y_predictions = ModelRNN(model_file_path='src/model_4.pkl').predict(df)
 
 # Save predictions to file
 df_predictions = pd.DataFrame({'prediction': y_predictions.flatten()}, index=df.index)
