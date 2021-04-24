c906ec6de2004ff141499101d13c6f042445a217
mateuszw314 <53479545+mateuszw314@users.noreply.github.com>
2021-04-24 13:56:09 +0200

    Use model2

--- src/predict.py
+++ src/predict.py
@@ -15,7 +15,7 @@ with open(args.input_csv) as input_csv:
 
 # Run predictions
 # y_predictions = BaselineModel(model_file_path='src/model.pickle').predict(df)
-y_predictions = ModelRNN(model_file_path='src/model.pkl').predict(df)
+y_predictions = ModelRNN(model_file_path='src/model_2.pkl').predict(df)
 
 # Save predictions to file
 df_predictions = pd.DataFrame({'prediction': y_predictions.flatten()}, index=df.index)
